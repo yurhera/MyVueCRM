3f4697ef2b39901fa5f9c6db74daad1e502106f8
Yuriy <yurjxxx@gmail.com>
2020-03-17 10:56:06 +0200

    add sidebar logic

---
+++ src/components/app/AddRecord.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="fixed-action-btn">
+        <a class="btn-floating btn-large blue" href="#">
+            <i class="large material-icons">add</i>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "add-record"
+    }
+</script>
+
+<style scoped>
+
+</style>
